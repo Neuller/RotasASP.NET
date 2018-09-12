1fd4bfd2d9b9549e28f3c4ace68ec8cd422f103e
Neuller <Neuller97@hotmail.com>
2018-09-12 08:41:14 -0300

    Projeto Trabalhando com Rotas em ASP.NET

--- Rotas/Controllers/ManageController.cs
+++ Rotas/Controllers/ManageController.cs
@@ -19,7 +19,7 @@ namespace Rotas.Controllers
         public ManageController()
         {
         }
-
+        
         public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
